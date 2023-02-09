da4a291d499888a951b261cb803bc89c1e72733e
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-09 18:50:33 -0500

    logo whatsapp

---
+++ includes/chatwp.php
@@ -0,0 +1,7 @@
+<div chass="whatsapp-container">
+    <div class="whatsapp-row">
+        <div class="">
+            <img src="images/logo/logo-whatsapp.png" alt="Whatsapp contacto" srcset="">
+        </div>
+    </div>
+</div>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '975703305d8cd897d1369eeed64b2be33e0734f2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '975703305d8cd897d1369eeed64b2be33e0734f2',
+            'dev_requirement' => false,
+        ),
+        'ultramsg/whatsapp-php-sdk' => array(
+            'pretty_version' => '2.0.7',
+            'version' => '2.0.7.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ultramsg/whatsapp-php-sdk',
+            'aliases' => array(),
+            'reference' => 'a4287d176d8ec1a2c44e62d4d178103e2cefa360',
+            'dev_requirement' => false,
+        ),
+    ),
+);
