ded1d91fd14d4a1c2868b5f2e36fcea21380b485
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-09 20:07:10 -0500

    logo whatsapp

--- includes/chatwp.php
+++ includes/chatwp.php
@@ -1,7 +1,9 @@
 <div chass="whatsapp-container">
     <div class="whatsapp-row">
         
-            <img src="images/logo/logo-whatsapp.png" alt="Whatsapp contacto" srcset="">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+                 <img src="images/logo/logo-whatsapp.png" alt="Whatsapp contacto" srcset="">
+            </a>
         
     </div>
 </div>
