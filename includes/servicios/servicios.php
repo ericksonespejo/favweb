55faa8d2ad6bf05d5feabfc2f99e23132c95fa76
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-23 13:36:34 -0500

    fd

--- includes/servicios/servicios.php
+++ includes/servicios/servicios.php
@@ -27,8 +27,9 @@
                         alt="Fabricación de puertas automáticas">
                     <h2>Fabricación</h2>
                     <p class="servi_list_info_text">Fabricamos cualquier tipo de puerta de garaje, residencial,
-                         centros comerciales, empresas mineras para cualquier medida y adaptada a cualquier 
-                        tipología de construcción. Nos adaptamos a las necesidades de nuestros clientes. Elija entre nuestro catálogo o 
+                        centros comerciales, empresas mineras para cualquier medida y adaptada a cualquier
+                        tipología de construcción. Nos adaptamos a las necesidades de nuestros clientes. Elija entre
+                        nuestro catálogo o
                         explíquenos su proyecto. Favessa le construye la puerta que necesita.</p>
                 </div>
             </div>
@@ -37,9 +38,10 @@
                     <img class="servi_list_info_icon" src="images/servicios/icons/team.png"
                         alt="Montaje e Instalación de puertas automáticas">
                     <h2>Montaje e Instalación</h2>
-                    <p class="servi_list_info_text">Nuestro equipo se ocupa de todo el proceso que incluye la fabricación, nuestros profesionales realizan 
+                    <p class="servi_list_info_text">Nuestro equipo se ocupa de todo el proceso que incluye la
+                        fabricación, nuestros profesionales realizan
                         la toma de medidas en su casa, tienda, centro comercial, empresa minera, residencia,
-                        la instalación y el montaje de las puertas y los automatismos de las puertas. Dejamos su puerta 
+                        la instalación y el montaje de las puertas y los automatismos de las puertas. Dejamos su puerta
                         funcionando desde el primer día..</p>
                 </div>
                 <div class="col-md-6 serv_list_img">
@@ -54,8 +56,10 @@
                         <img class="servi_list_info_icon" src="images/servicios/icons/tools.png"
                             alt="Reparación de puertas automáticas">
                         <h2>Reparaciones</h2>
-                        <p class="servi_list_info_text">Somos fabricantes y por tanto conocemos la mecánica de cualquier tipo de puerta de garaje, centro comercial, residencia, emprea minera. 
-                            Nuestros especialistas están capacitados para reparar cualquier puerta automática de garaje o mecanismo de la competencia. 
+                        <p class="servi_list_info_text">Somos fabricantes y por tanto conocemos la mecánica de cualquier
+                            tipo de puerta de garaje, centro comercial, residencia, emprea minera.
+                            Nuestros especialistas están capacitados para reparar cualquier puerta automática de garaje
+                            o mecanismo de la competencia.
                             No hay secretos para nuestros técnicos.</p>
                     </div>
                 </div>
@@ -64,9 +68,11 @@
                         <img class="servi_list_info_icon" src="images/servicios/icons/maintenance.png"
                             alt="Mantenimiento de puertas automáticas">
                         <h2>Mantenimiento</h2>
-                        <p class="servi_list_info_text">Nuestro especialista de Favessa le ofrece un servicio profesional para que la puerta de garaje de su 
-                            vivienda o negocio o centro comercial o empresa minera no sea nunca un problema. 
-                            Nos ocupamos de su mantenimiento para que esté en óptimas condiciones todos los días del año, sin excepciones.</p>
+                        <p class="servi_list_info_text">Nuestro especialista de Favessa le ofrece un servicio
+                            profesional para que la puerta de garaje de su
+                            vivienda o negocio o centro comercial o empresa minera no sea nunca un problema.
+                            Nos ocupamos de su mantenimiento para que esté en óptimas condiciones todos los días del
+                            año, sin excepciones.</p>
                     </div>
                     <div class="col-md-6 serv_list_img">
                         <img src="images/servicios/mantenimiento-de-puertas-automaticas.webp"
@@ -75,8 +81,16 @@
                 </div>
             </div>
 
-    </div>
+        </div>
 
+</section>
+<section>
+    <div class="jumbotron">
+        <h1>Atendemos emergencias las 24 horas del día</h1>
+        <p>Contamos con personal técnico profesional eficaces que se 
+            encuentran en constante capacitación y a la vanguardia para  cubrir y dar soluciones de cualquier emergencia de nuestro cliente.</p>
+        <p><a class="btn btn-primary btn-lg" href="tel:+51948500076" role="button">Llamar ahora</a></p>
+    </div>
 </section>
 
 <!-- Start Client Section -->
