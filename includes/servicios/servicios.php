26f0cfa6296c17d68001d251d2b5527fa13ed031
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-22 20:00:52 -0500

    algo

--- includes/servicios/servicios.php
+++ includes/servicios/servicios.php
@@ -1,15 +1,30 @@
 <?php $migaPan="Nuestros servicios";?>
 
-        <!-- Start Header Section -->
-        <?php include_once 'includes/miga-pan.php'; ?>
-        <!-- End Header Section -->
-        
-        
-        
-        
-        
-        
-        <!-- Start Client Section -->
-        <?php include_once 'includes/inicio/clientes-logos.php';?>
-        <!-- End Client Section -->
-        
+<!-- Start Header Section -->
+<?php include_once 'includes/miga-pan.php'; ?>
+<!-- End Header Section -->
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                <h2 class="hvr-underline-from-center">¿Que te Ofrecemos?</h2>
+                <p>Favessa Expertos en puertas automáticas</p>
+            </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="" alt="" srcset="">
+            </div>
+            <div class="col-md-6">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate velit iste nobis, reprehenderit facilis ducimus. Quasi, nostrum, rem pariatur ad officiis praesentium sequi consectetur dolore, veniam enim quisquam impedit animi!
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Start Client Section -->
+<?php include_once 'includes/inicio/clientes-logos.php';?>
+<!-- End Client Section -->
