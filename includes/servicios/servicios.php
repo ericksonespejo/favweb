127dc834677392464044cd3e650bf529a9043ca7
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-23 12:34:57 -0500

    kiy

--- includes/servicios/servicios.php
+++ includes/servicios/servicios.php
@@ -4,7 +4,7 @@
 <?php include_once 'includes/miga-pan.php'; ?>
 <!-- End Header Section -->
 
-<section class="pattern_bg">
+<section>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -14,55 +14,68 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 serv_list_img">
-                <img src="images/servicios/fabricacion-de-puertas-automaticas.jpg" alt="Fabricación de puertas levadisas">
+        <div class="pattern_bg">
+
+            <div class="row">
+                <div class="col-md-6 serv_list_img">
+                    <img src="images/servicios/fabricacion-de-puertas-automaticas.jpg"
+                        alt="Fabricación de puertas levadisas">
+                </div>
+                <div class="col-md-6 servi_list_info">
+                    <img class="servi_list_info_icon" src="images/servicios/icons/factory.png"
+                        alt="Fabricación de puertas automáticas">
+                    <h2>Fabricación</h2>
+                    <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                        vero beatae optio iusto fugit.
+                        Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
+                        reiciendis minima, facilis ab.</p>
+                </div>
             </div>
-            <div class="col-md-6 servi_list_info">
-                <img class="servi_list_info_icon" src="images/servicios/icons/factory.png" alt="Fabricación de puertas automáticas">
-                <h2>Fabricación</h2>
-                <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vero beatae optio iusto fugit.
-                    Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
-                    reiciendis minima, facilis ab.</p>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6 servi_list_info">
-                <img class="servi_list_info_icon" src="images/servicios/icons/team.png" alt="Montaje e Instalación de puertas automáticas">
-                <h2>Montaje e Instalación</h2>
-                <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vero beatae optio iusto fugit.
-                    Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
-                    reiciendis minima, facilis ab.</p>
-            </div>
-            <div class="col-md-6 serv_list_img">
-                <img src="images/servicios/garage-door.jpg" alt="Puertas de garage"">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6 serv_list_img">
-                <img src="images/servicios/Automatic-Door-repair.jpg" alt="Reparación de puertas automáticas"">
-            </div>
-            <div class="col-md-6 servi_list_info">
-                <img class="servi_list_info_icon" src="images/servicios/icons/tools.png" alt="Reparación de puertas automáticas">
-                <h2>Reparaciones</h2>
-                <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vero beatae optio iusto fugit.
-                    Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
-                    reiciendis minima, facilis ab.</p>
+            <div class="row">
+                <div class="col-md-6 servi_list_info">
+                    <img class="servi_list_info_icon" src="images/servicios/icons/team.png"
+                        alt="Montaje e Instalación de puertas automáticas">
+                    <h2>Montaje e Instalación</h2>
+                    <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                        vero beatae optio iusto fugit.
+                        Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
+                        reiciendis minima, facilis ab.</p>
+                </div>
+                <div class="col-md-6 serv_list_img">
+                    <img src="images/servicios/garage-door.jpg" alt="Puertas de garage"">
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 servi_list_info">
-                <img class="servi_list_info_icon" src="images/servicios/icons/maintenance.png" alt="Mantenimiento de puertas automáticas">
-                <h2>Mantenimiento</h2>
-                <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam vero beatae optio iusto fugit.
-                    Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
-                    reiciendis minima, facilis ab.</p>
+        <div class=" row">
+                    <div class="col-md-6 serv_list_img">
+                        <img src="images/servicios/Automatic-Door-repair.jpg" alt="Reparación de puertas automáticas"">
             </div>
-            <div class="col-md-6 serv_list_img">
-                <img src="images/servicios/mantenimiento-de-puertas-automaticas.webp" alt="Mantenimiento de puertas automáticas">
+            <div class=" col-md-6 servi_list_info">
+                        <img class="servi_list_info_icon" src="images/servicios/icons/tools.png"
+                            alt="Reparación de puertas automáticas">
+                        <h2>Reparaciones</h2>
+                        <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quisquam vero beatae optio iusto fugit.
+                            Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
+                            reiciendis minima, facilis ab.</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 servi_list_info">
+                        <img class="servi_list_info_icon" src="images/servicios/icons/maintenance.png"
+                            alt="Mantenimiento de puertas automáticas">
+                        <h2>Mantenimiento</h2>
+                        <p class="servi_list_info_text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Quisquam vero beatae optio iusto fugit.
+                            Expedita iure, accusamus illum recusandae provident adipisci excepturi quae est error natus
+                            reiciendis minima, facilis ab.</p>
+                    </div>
+                    <div class="col-md-6 serv_list_img">
+                        <img src="images/servicios/mantenimiento-de-puertas-automaticas.webp"
+                            alt="Mantenimiento de puertas automáticas">
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 </section>
 
 <!-- Start Client Section -->
