26151c031e0b7a7dacd4ec6fc4b5e1791666b259
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-23 19:05:07 -0500

    kuyt

--- includes/servicios/servicios.php
+++ includes/servicios/servicios.php
@@ -85,7 +85,7 @@
 
 </section>
 <section>
-    <div class="jumbotron centrado_todo">
+    <div class="jumbotron centrado_todo label-success">
         <h1>Atendemos emergencias las 24 horas del día</h1>
         <p>Contamos con personal técnico profesional eficaces que se 
             encuentran en constante capacitación<br> y a la vanguardia para  cubrir y dar soluciones de cualquier emergencia de nuestro cliente.</p>
