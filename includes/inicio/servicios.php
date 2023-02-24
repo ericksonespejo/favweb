9b48c521e36979a9d8ea2d0ce35e420597e55eb1
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-23 19:17:15 -0500

    kuyt

--- includes/inicio/servicios.php
+++ includes/inicio/servicios.php
@@ -1,59 +1,9 @@
 
-<!-- Start Service Section -->
-<section id="service-section">
-    <div class="container">
-    <div class="row">
-            <div class="col-md-12">
-                <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
-                    <h2 class="hvr-underline-from-center">Nuestros Servicios</h2>
-                    <p>Realizamos venta de motores, fabricación, instalación y atención de emergencias las 24 horas</p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-skyatlas"></i></a>
-                    <h2>Venta de motores para puertas automaticas</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-magic"></i></a>
-                    <h2>Atención de emergencias las 24 horas</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-gift"></i></a>
-                    <h2>Instalación de puertas automáticas</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-diamond"></i></a>
-                    <h2>Mantenimiento de puertas automáticas</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-wordpress"></i></a>
-                    <h2>RESPONSIVE DESIGN</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="services-post">
-                    <a href="#"><i class="fa fa-forumbee"></i></a>
-                    <h2>RESPONSIVE DESIGN</h2>
-                    <p>Donec odio. Quisque volutpat mattis eros. Nullam malesuada </p>
-                </div>
-            </div>
-        </div>
+<section>
+    <div class="jumbotron centrado_todo bg-jumbotron">
+        <h1>Atendemos emergencias las 24 horas del día</h1>
+        <p>Contamos con personal técnico profesional eficaces que se 
+            encuentran en constante capacitación<br> y a la vanguardia para  cubrir y dar soluciones de cualquier emergencia de nuestro cliente.</p>
+        <p><a class="btn btn-primary btn-lg" href="tel:+51948500076" role="button">Llamar ahora</a></p>
     </div>
 </section>
-<!-- Start Service Section -->
