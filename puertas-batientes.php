db8d5a9e38050a28d172d8d553c57806655460f2
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-25 13:25:25 -0500

    kion

--- puertas-batientes.php
+++ puertas-batientes.php
@@ -27,39 +27,11 @@
                         </div>
                         <h1 class="post-title"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h1>
                         
-                        <div class="post-meta">
-                            <ul class=" pull-left">
-                                <li><i class="fa fa-clock-o"></i>October 25 2014</li>
-                                <li><i class="fa fa-user"></i><a href="#">Super User</a></li>
-                                <li><i class="fa fa-file"></i><a href="#">Blog</a></li>
-                                <li><i class="fa fa-tags"></i><a href="#">Women Rights</a></li>
-                                <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
-                            </ul>
-                            <ul class="pull-right">
-                                <li>font-size<i class="fa fa-minus-circle"></i><i class="fa fa-plus-circle"></i></li>
-                                <li><a href="#">Print</a></li>
-                                <li><a href="#">Email</a></li>
-                            </ul>
-                        </div>
-                        
                         
                         <p class="post-content">Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo.</p>
                         <p class="post-content">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.</p>
                         <p class="post-content">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. </p>
                         
-                        <div class="item-content-footer">
-                            <ul>
-                                <li>Read 286 times</li>
-                                <li class="rating">
-                                    Rate this item 
-                                    <span class="active"><i class="fa fa-star-o"></i></span>
-                                    <span class="active"><i class="fa fa-star-o"></i></span>
-                                    <span class="active"><i class="fa fa-star-o"></i></span>
-                                    <span><i class="fa fa-star-o"></i></span>
-                                    <span><i class="fa fa-star-o"></i></span>
-                                </li>
-                            </ul>
-                        </div>
                         
                     </div>
                     <!-- End Blog Post -->
@@ -81,106 +53,6 @@
                         </div>
                     </div>
                     <!-- End Blog Author Section -->
-                    
-                    <!-- Start Comments Section -->
-                    <div id="comments">
-                        <h2 class="comments-title">4 comments</h2>
-                        <ol class="comments-list">
-                            <li>
-                                <div class="comment-box clearfix">
-                                    <div class="avatar"><img alt="" src="images/user-2.png"></div>
-                                    <div class="comment-content">
-                                        <div class="comment-meta">
-                                            <span class="comment-by">fghfghjfghj</span>
-                                            <span class="reply-link pull-right"><a href="#">Comment Link</a></span>
-                                        </div>
-                                        <span class="comment-date">Sunday, 15 February 2015 at 20:39</span>
-                                        <p>Dette er en besked</p>
-                                    </div>
-                                </div>
-     
-                                <ul>
-                                    <li>
-                                        <div class="comment-box clearfix">
-                                            <div class="avatar"><img alt="" src="images/user-2.png"></div>
-                                            <div class="comment-content">
-                                                <div class="comment-meta">
-                                                    <span class="comment-by">sdfaasdfb</span>
-                                                    <span class="reply-link pull-right"><a href="#">Comment Link</a></span>
-                                                </div>
-                                                <span class="comment-date">Sunday, 15 February 2015 20:39</span>
-                                                <p>sdfadasdfawefasdf a</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-  
-                            <li>
-                                <div class="comment-box clearfix">
-                                    <div class="avatar"><img alt="" src="images/user-2.png"></div>
-                                    <div class="comment-content">
-                                        <div class="comment-meta">
-                                            <span class="comment-by">sa</span>
-                                            <span class="reply-link pull-right"><a href="#">Comment Link</a></span>
-                                        </div>
-                                        <span class="comment-date">Tuesday, 03 February 2015 06:41</span>
-                                        <p>daaass</p>
-                                    </div>
-                                </div>
-                                
-                                <ul>
-                                    <li>
-                                        <div class="comment-box clearfix">
-                                            <div class="avatar"><img alt="" src="images/user-2.png"></div>
-                                            <div class="comment-content">
-                                                <div class="comment-meta">
-                                                    <span class="comment-by">xc zc</span>
-                                                    <span class="reply-link pull-right"><a href="#">Comment Link</a></span>
-                                                </div>
-                                                <span class="comment-date">Wednesday, 10 December 2014 11:42</span>
-                                                <p>fsfsfs</p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
- 
-                        </ol>
-                        
-                        <!-- Start Respond Form -->
-                        <div id="respond">
-                            <h2 class="respond-title">Leave a comment</h2>
-                            <span class="form-caution">Make sure you enter the (*) required information where indicated. HTML code is not allowed.</span>
-                            <form action="#">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <label for="author">Name<span class="required">*</span></label>
-                                        <input id="author" name="author" type="text" size="30" aria-required="true">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="email">Email<span class="required">*</span></label>
-                                        <input id="email" name="author" type="text" size="30" aria-required="true">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="url">Website<span class="required">*</span></label>
-                                        <input id="url" name="url" type="text" size="30" aria-required="true">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label for="comment">Add Your Comment</label>
-                                        <textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
-                                        <input name="submit" type="submit" id="submit" class="btn btn-primary" value="Submit Comment">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- End Respond Form -->
-
-                    </div>
-                    <!-- End Comments Section -->
-                    
                     
                     
                     
