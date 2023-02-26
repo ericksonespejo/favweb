504f7ca20552365ef3203b04bd560e856d78f730
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-25 21:10:43 -0500

    kion

--- puertas-enrollables.php
+++ puertas-enrollables.php
@@ -11,123 +11,134 @@
 <?php include_once 'includes/miga-pan.php'; ?>
 <!-- End Header Section -->
 
- 
-        <!-- Start Blog Page Section -->
-        <div class="container">
-            <div class="row">
-                
-                <!-- Start Blog Body Section -->
-                <div class="col-md-8 blog-body">
-                    
-                    <!-- Start Blog post -->
-                    <div class="single-blog-post">
-                        <div class="post-img">
-                            <img src="images/blog/blog-02.jpg" class="img-responsive" alt="Blog image">
-                        </div>
-                        <h1 class="post-title"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h1>
-                        
-                        <p class="post-content">Las puertas enrollables son la solución perfecta para ahorrar espacio ,  ya que su apertura se realiza mediante el enrollamiento de la cortina metálica en la parte superior de la puerta.
-</p>
-                        <p class="post-content">Fabricamos nuestra puertas con balletas  de acero galvanizado.  Nuestras puertas son de alta resistencia a la corrosión , interperie , de gran resistencia mecánica y de larga duración al desgaste  , por lo que aseguran una gran durabilidad , no requiere de mayor mantenimiento. Ideal para locales comerciales , almacenes y residenciales 
-</p>
-                        <p class="post-content">En FAVESSA adaptamos la fabricación de nuestras puertas de ambientes industriales a las medias que necesites y ofrecemos una amplia gama de accesorios a nivel de activación , control y seguridad
-</p>
-                        
-                        
-                    </div>
-                    <!-- End Blog Post -->
-                    
-                    
-                    <!-- Start Blog Author Section -->
-                    <div class="blog-author">
-                        <h3>About The Author</h3>
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/user.png" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Super User</a></h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End Blog Author Section -->
-                    
-                    
-                    
-                    
-                    
+
+<!-- Start Blog Page Section -->
+<div class="container">
+    <div class="row">
+
+        <!-- Start Blog Body Section -->
+        <div class="col-md-8 blog-body">
+
+            <!-- Start Blog post -->
+            <div class="single-blog-post">
+                <div class="post-img">
+                    <img src="images/blog/blog-02.jpg" class="img-responsive" alt="Blog image">
                 </div>
-                <!-- End Blog Body Section -->
-                
-                <!-- Start Sidebar Section -->
-                <div class="col-md-4 sidebar right-sidebar">
-                    
-                    
-                    <!-- Start Recent Post Widget -->
-                    <div class="widget widget-recent-post">
-                        
-                        <div class="section-heading-2">
-                            <h3 class="section-title">
-                                <span>Recent Post</span>
-                            </h3>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-02.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-03.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Quisque cursus metus vitae pharetra auctor sem massa</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-04.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Praesent libero sed cursus ante dapibus diam</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <!-- End Recent Post Widget -->                    
-                </div>
-                <!-- End Sidebar Section -->
-                
+                <h1 class="post-title"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h1>
+
+                <p class="post-content">Las puertas enrollables son la solución perfecta para ahorrar espacio , ya que
+                    su apertura se realiza mediante el enrollamiento de la cortina metálica en la parte superior de la
+                    puerta.
+                </p>
+                <p class="post-content">Fabricamos nuestra puertas con balletas de acero galvanizado. Nuestras puertas
+                    son de alta resistencia a la corrosión , interperie , de gran resistencia mecánica y de larga
+                    duración al desgaste , por lo que aseguran una gran durabilidad , no requiere de mayor
+                    mantenimiento. Ideal para locales comerciales , almacenes y residenciales
+                </p>
+                <p class="post-content">En FAVESSA adaptamos la fabricación de nuestras puertas de ambientes
+                    industriales a las medias que necesites y ofrecemos una amplia gama de accesorios a nivel de
+                    activación , control y seguridad
+                </p>
+
+
             </div>
+            <!-- End Blog Post -->
+
+
+            <!-- Start Blog Author Section -->
+            <div class="blog-author">
+                <h3>About The Author</h3>
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object" src="images/user.png" alt="...">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="#">Super User</a></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Blog Author Section -->
+
+
+
+
+
         </div>
-        <!-- End Blog Page Section -->
+        <!-- End Blog Body Section -->
+
+        <!-- Start Sidebar Section -->
+        <div class="col-md-4 sidebar right-sidebar">
+
+
+            <!-- Start Recent Post Widget -->
+            <div class="widget widget-recent-post">
+
+                <div class="section-heading-2">
+                    <h3 class="section-title">
+                        <span>Recent Post</span>
+                    </h3>
+                </div>
+
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object" src="images/recent-post/post-02.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h4>
+                        <ul>
+                            <li><a href="#">Super User</a></li>
+                            <li>15 October 2014</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object" src="images/recent-post/post-03.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="#">Quisque cursus metus vitae pharetra auctor sem massa</a>
+                        </h4>
+                        <ul>
+                            <li><a href="#">Super User</a></li>
+                            <li>15 October 2014</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="media">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object" src="images/recent-post/post-04.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="#">Praesent libero sed cursus ante dapibus diam</a></h4>
+                        <ul>
+                            <li><a href="#">Super User</a></li>
+                            <li>15 October 2014</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <!-- End Recent Post Widget -->
+        </div>
+        <!-- End Sidebar Section -->
+
+    </div>
+</div>
+<!-- End Blog Page Section -->
 
 
 <?php include_once 'includes/footer.php'; ?>
