71b51f052c62becd2d6234115c82c14a83f0a4b9
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-25 21:24:12 -0500

    kion

--- puertas-corredizas.php
+++ puertas-corredizas.php
@@ -11,102 +11,57 @@
 <?php include_once 'includes/miga-pan.php'; ?>
 <!-- End Header Section -->
 
- 
-        <!-- Start Blog Page Section -->
-        <div class="container">
-            <div class="row">
-                
-                <!-- Start Blog Body Section -->
-                <div class="col-md-8 blog-body">
-                    
-                    <!-- Start Blog post -->
-                    <div class="single-blog-post">
-                        <div class="post-img">
-                            <img src="images/blog/blog-02.jpg" class="img-responsive" alt="Blog image">
-                        </div>
-                        <h1 class="post-title"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h1>
-                        
-                        <p class="post-content">Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo.</p>
-                        <p class="post-content">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.</p>
-                        <p class="post-content">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. </p>
-                      
-                        
-                    </div>
-                    <!-- End Blog Post -->
-                    
-                    
-                    <?php include_once 'includes/sobre-favessa.php'; ?>
-                    
+
+<!-- Start Blog Page Section -->
+<div class="container">
+    <div class="row">
+
+        <!-- Start Blog Body Section -->
+        <div class="col-md-8 blog-body">
+
+            <!-- Start Blog post -->
+            <div class="single-blog-post">
+                <div class="post-img">
+                    <img src="images/blog/blog-02.jpg" class="img-responsive" alt="Blog image">
                 </div>
-                <!-- End Blog Body Section -->
-                
-                <!-- Start Sidebar Section -->
-                <div class="col-md-4 sidebar right-sidebar">
-                    
-                    
-                    <!-- Start Recent Post Widget -->
-                    <div class="widget widget-recent-post">
-                        
-                        <div class="section-heading-2">
-                            <h3 class="section-title">
-                                <span>Recent Post</span>
-                            </h3>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-02.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-03.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Quisque cursus metus vitae pharetra auctor sem massa</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                        <div class="media">
-                            <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="images/recent-post/post-04.jpg" alt="...">
-                                </a>
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Praesent libero sed cursus ante dapibus diam</a></h4>
-                                <ul>
-                                    <li><a href="#">Super User</a></li>
-                                    <li>15 October 2014</li>
-                                </ul>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    <!-- End Recent Post Widget -->
-                    
-                </div>
-                <!-- End Sidebar Section -->
-                
+                <h1 class="post-title"><a href="#">Nulla facilisi integer lacinia sollicitudin massa</a></h1>
+
+                <p class="post-content">Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet
+                    risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in,
+                    ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum
+                    nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus
+                    quis, aliquet eget, justo.</p>
+                <p class="post-content">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum
+                    magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+                    cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet
+                    augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra
+                    nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices
+                    enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper.</p>
+                <p class="post-content">Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                    himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a
+                    cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a
+                    tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu
+                    magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. </p>
+
+
             </div>
+            <!-- End Blog Post -->
+
+
+            <?php include_once 'includes/sobre-favessa.php'; ?>
+
         </div>
-        <!-- End Blog Page Section -->
+        <!-- End Blog Body Section -->
+
+        <!-- Start Sidebar Section -->
+        <div class="col-md-4 sidebar right-sidebar">
+            <?php include_once 'includes/post-recientes.php'; ?>
+        </div>
+        <!-- End Sidebar Section -->
+
+    </div>
+</div>
+<!-- End Blog Page Section -->
 
 
 
