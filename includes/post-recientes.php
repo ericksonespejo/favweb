95270281874a4b9fd8255329ab210d820bb8125b
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-27 23:41:03 -0500

    asi se avanza

--- includes/post-recientes.php
+++ includes/post-recientes.php
@@ -10,11 +10,11 @@
     <div class="media">
         <div class="media-left">
             <a href="#">
-                <img class="media-object" src="images/recent-post/post-02.jpg" alt="...">
+                <img class="media-object" src="images/recent-post/post-02.jpg" alt="Puertas automáticas batientes">
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Puertas automáticas batientes</a></h4>
+            <h4 class="media-heading"><a href="puertas-automaticas-batientes.php">Puertas automáticas batientes</a></h4>
             
         </div>
     </div>
@@ -22,11 +22,11 @@
     <div class="media">
         <div class="media-left">
             <a href="#">
-                <img class="media-object" src="images/recent-post/post-03.jpg" alt="...">
+                <img class="media-object" src="images/recent-post/post-03.jpg" alt="Puertas automáticas enrollables">
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Puertas autmáticas enrollables</a></h4>
+            <h4 class="media-heading"><a href="puertas-automaticas-enrollables.php">Puertas autmáticas enrollables</a></h4>
            
         </div>
     </div>
@@ -34,11 +34,22 @@
     <div class="media">
         <div class="media-left">
             <a href="#">
-                <img class="media-object" src="images/recent-post/post-04.jpg" alt="...">
+                <img class="media-object" src="images/recent-post/post-04.jpg" alt="Puertas automáticas levadizas">
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Puertas enrollables levadizas</a></h4>
+            <h4 class="media-heading"><a href="puertas-automaticas-levadizas.php">Puertas automáticas levadizas</a></h4>
+           
+        </div>
+    </div>
+    <div class="media">
+        <div class="media-left">
+            <a href="#">
+                <img class="media-object" src="images/recent-post/post-04.jpg" alt="Puertas automáticas seccionales">
+            </a>
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading"><a href="puertas-automaticas-seccionales.php">Puertas automáticas seccionales</a></h4>
            
         </div>
     </div>
@@ -49,7 +60,7 @@
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Puertas enrollables seccionales</a></h4>
+            <h4 class="media-heading"><a href="puertas-industriales.php">Puertas industriales</a></h4>
            
         </div>
     </div>
@@ -60,7 +71,7 @@
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Puertas industriales</a></h4>
+            <h4 class="media-heading"><a href="motores-de-puertas-automaticas.php">Ventas de motores para puertas automáticas</a></h4>
            
         </div>
     </div>
@@ -71,18 +82,7 @@
             </a>
         </div>
         <div class="media-body">
-            <h4 class="media-heading"><a href="#">Ventas de motores para puertas automáticas</a></h4>
-           
-        </div>
-    </div>
-    <div class="media">
-        <div class="media-left">
-            <a href="#">
-                <img class="media-object" src="images/recent-post/post-04.jpg" alt="...">
-            </a>
-        </div>
-        <div class="media-body">
-            <h4 class="media-heading"><a href="#">Venta de paneles</a></h4>
+            <h4 class="media-heading"><a href="venta-de-paneles.php">Venta de paneles</a></h4>
            
         </div>
     </div>
---
+++ venta-de-paneles.php
@@ -0,0 +1,16 @@
+<?php $title="Favessa, somos especialistas en puertas enrollables automaticas";?>
+<?php $description="Fabricamos puertas enrollables a medida para centros comerciales, tiendas, minerias";?>
+<?php include_once 'includes/head.php';?>
+<?php include_once 'includes/header.php'?>
+
+
+<!-- Start Header Section -->
+<?php $migaPan="Venta de Motores para puertas automaticas";?>
+
+<!-- Start Header Section -->
+<?php include_once 'includes/miga-pan.php'; ?>
+<!-- End Header Section -->
+
+
+
+<?php include_once 'includes/footer.php'; ?>
