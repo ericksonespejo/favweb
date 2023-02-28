8f906372690a03f71868be636263efc1e8af62e3
Erickson Espejo <espejo.erickson@gmail.com>
2023-02-28 17:31:46 -0500

    gallery

--- puertas-automaticas-seccionales.php
+++ puertas-automaticas-seccionales.php
@@ -53,11 +53,11 @@
 				<ul class="lb-album">
 					<li>
 						<a href="#image-1">
-							<img src="images/thumbs/1.jpg" alt="image01">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/5.jpgg" alt="image01">
 							<span>Pointe</span>
 						</a>
 						<div class="lb-overlay" id="image-1">
-							<img src="images/full/1.jpg" alt="image01">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/1.jpg" alt="image01">
 							<div>
 								<h3>pointe <span>/point/</span></h3>
 								<p>Dance performed on the tips of the toes</p>
@@ -67,11 +67,11 @@
 					</li>
 					<li>
 						<a href="#image-2">
-							<img src="images/thumbs/2.jpg" alt="image02">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/2.jpg" alt="image02">
 							<span>Port de bras</span>
 						</a>
 						<div class="lb-overlay" id="image-2">
-							<img src="images/full/2.jpg" alt="image02">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/2.jpg" alt="image02">
 							<div>							
 								<h3>port de bras <span>/ˌpôr də ˈbrä/</span></h3>
 								<p>An exercise designed to develop graceful movement and disposition of the arms</p>
@@ -81,11 +81,11 @@
 					</li>
 					<li>
 						<a href="#image-3">
-							<img src="images/thumbs/3.jpg" alt="image03">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/3.jpg" alt="image03">
 							<span>Plié</span>
 						</a>
 						<div class="lb-overlay" id="image-3">
-							<img src="images/full/3.jpg" alt="image03">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/3.jpg" alt="image03">
 							<div>							
 								<h3>pli·é <span>/plēˈā/</span></h3>
 								<p>A movement in which a dancer bends the knees and straightens them again</p>
@@ -95,11 +95,11 @@
 					</li>
 					<li>
 						<a href="#image-4">
-							<img src="images/thumbs/4.jpg" alt="image04">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/4.jpg" alt="image04">
 							<span>Adagio</span>
 						</a>
 						<div class="lb-overlay" id="image-4">
-							<img src="images/full/4.jpg" alt="image04">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/4.jpg" alt="image04">
 							<div>							
 								<h3>a·da·gio <span>/əˈdäjō/</span></h3>
 								<p>A movement or composition marked to be played adagio</p>
@@ -109,11 +109,11 @@
 					</li>
 					<li>
 						<a href="#image-5">
-							<img src="images/thumbs/5.jpg" alt="image05">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/5.jpg" alt="image05">
 							<span>Frappé</span>
 						</a>
 						<div class="lb-overlay" id="image-5">
-							<img src="images/full/5.jpg" alt="image05">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/5.jpg" alt="image05">
 							<div>							
 								<h3>frap·pé<span>/fraˈpā/</span></h3>
 								<p>Involving a beating action of the toe of one foot against the ankle of the supporting leg</p>
@@ -123,11 +123,11 @@
 					</li>
 					<li>
 						<a href="#image-6">
-							<img src="images/thumbs/6.jpg" alt="image06">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/6.jpg" alt="image06">
 							<span>Glissade</span>
 						</a>
 						<div class="lb-overlay" id="image-6">
-							<img src="images/full/6.jpg" alt="image06">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/6.jpg" alt="image06">
 							<div>							
 								<h3>glis·sade <span>/gliˈsäd/</span></h3>
 								<p>One leg is brushed outward from the body, which then takes the weight while the second leg is brushed in to meet it</p>
@@ -137,11 +137,11 @@
 					</li>
 					<li>
 						<a href="#image-7">
-							<img src="images/thumbs/7.jpg" alt="image07">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/7.jpg" alt="image07">
 							<span>Jeté</span>
 						</a>
 						<div class="lb-overlay" id="image-7">
-							<img src="images/full/7.jpg" alt="image07">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/7.jpg" alt="image07">
 							<div>							
 								<h3>je·té <span>/zhə-ˈtā/</span></h3>
 								<p>A springing jump made from one foot to the other in any direction</p>
@@ -151,11 +151,11 @@
 					</li>
 					<li>
 						<a href="#image-8">
-							<img src="images/thumbs/8.jpg" alt="image08">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/8.jpg" alt="image08">
 							<span>Piqué</span>
 						</a>
 						<div class="lb-overlay" id="image-8">
-							<img src="images/full/8.jpg" alt="image08">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/8.jpg" alt="image08">
 							<div>							
 								<h3>pi·qué <span>/pēˈkā/</span></h3>
 								<p>Strongly pointed toe of the lifted and extended leg sharply lowers to hit the floor then immediately rebounds upward</p>
@@ -165,11 +165,11 @@
 					</li>
 					<li>
 						<a href="#image-9">
-							<img src="images/thumbs/9.jpg" alt="image09">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/9.jpg" alt="image09">
 							<span>Arabesque</span>
 						</a>
 						<div class="lb-overlay" id="image-9">
-							<img src="images/full/9.jpg" alt="image09">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/9.jpg" alt="image09">
 							<div>							
 								<h3>ar·a·besque <span>/ˌarəˈbesk/</span></h3>
 								<p>Position of the body supported on one leg, with the other leg extended behind the body with the knee straight</p>
@@ -179,11 +179,11 @@
 					</li>
 					<li>
 						<a href="#image-10">
-							<img src="images/thumbs/10.jpg" alt="image10">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/thumbs/10.jpg" alt="image10">
 							<span>Ballerina</span>
 						</a>
 						<div class="lb-overlay" id="image-10">
-							<img src="images/full/10.jpg" alt="image10">
+							<img src="https://tympanus.net/Tutorials/CSS3Lightbox/images/full/10.jpg" alt="image10">
 							<div>							
 								<h3>bal·le·ri·na <span>/ˌbaləˈrēnə/</span></h3>
 								<p>A female ballet dancer</p>
